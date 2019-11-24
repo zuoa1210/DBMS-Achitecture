99870c14ed711a86d2914161c39cf8d4de87a60a
zuoa1210 <46008527+zuoa1210@users.noreply.github.com>
2019-11-24 10:59:53 -0500

    Add files via upload

---
+++ SQLParser/InsertSqlParser.java
@@ -0,0 +1,23 @@
+package SqlParse;
+
+public class InsertSqlParser extends BaseSqlParser{
+
+	public InsertSqlParser(String originalSql) {
+		super(originalSql);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initializeSegments() {
+		// TODO Auto-generated method stub
+		segments.add(new SqlSegment("(insert into)(.+?)(values)","[,]"));
+		//segments.add(new SqlSegment("([(])(.+?)([)] values [(])","[,]"));
+		segments.add(new SqlSegment("(values [(])(.+)([)] ENDOFSQL)","[,]"));
+	}
+
+	public String getParsedSql() {
+		String retval=super.getParsedSql();
+		retval=retval+")";
+		return retval;
+	}
+}
