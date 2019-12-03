8422ba1fdef5942e509068f98c6fd6a9d04c1888
zuoa1210 <46008527+zuoa1210@users.noreply.github.com>
2019-12-02 19:38:09 -0500

    Add files via upload

--- SQLParser/CreateSqlParser.java
+++ SQLParser/CreateSqlParser.java
@@ -10,10 +10,13 @@ public class CreateSqlParser extends BaseSqlParser{
 	@Override
 	protected void initializeSegments() {
 		// TODO Auto-generated method stub
-		segments.add(new SqlSegment("(create table)(.+)([(])","[,]"));
+		segments.add(new SqlSegment("(create table)(.+?)([(])","[,]"));
 		segments.add(new SqlSegment("(create index)(.+)(on)","[,]"));
 		segments.add(new SqlSegment("(on)(.+)([(])", "[,]"));
 		segments.add(new SqlSegment("([(])(.+)([)] ENDOFSQL)","[,]"));
+		segments.add(new SqlSegment("(primary key[(])(.+?)([)])", "[,]"));
+		segments.add(new SqlSegment("(foreign key[(])(.+?)([)] references)", "[,]"));
+		segments.add(new SqlSegment("(references)(.+?)([)] ENDOFSQL)", "[,]"));
 	}
-
+	
 }
--- SQLParser/SqlSegment.java
+++ SQLParser/SqlSegment.java
@@ -61,7 +61,16 @@ public class SqlSegment {
 		start = start.trim();
 		
 		ls.add(start);
-		String[] arr = sb.toString().split("[|]");
+		String s = sb.toString();
+
+		if(s.indexOf("primary key(") != -1) {
+			s = s.substring(0, s.indexOf("primary key(") - 1);
+		} 
+		if(s.indexOf("foreign key(") != -1) {
+			s = s.substring(0, s.indexOf("foreign key(") - 1);
+		} 
+
+		String[] arr = s.split("[|]");
 		int arrLength = arr.length;
 		for(int i = 0; i < arrLength; i++) {
 			ls.add(arr[i]);
